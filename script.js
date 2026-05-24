6acc62b9b317ea55d4c53946157dbe566de5feed
vilela italo <batistaitalo642@gmail.com>
2026-05-24 10:07:33 -0300

    segundo commit do js

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-let conta = 0
-let pessoas = 0
+    let conta = 0
+    let pessoas = 0
+    let porcentagem = 0
+    const contaInput = document.querySelector("#conta")
+    contaInput.addEventListener("input", receberValorConta)
 
-const contaInput = document.querySelector("#conta")
-contaInput.addEventListener("input", receberValorConta)
-
-function receberValorConta(evento) {
-    conta = Number(evento.target.value)
-}
-
-const pessoasInput = document.querySelector("#pessoas")
-pessoasInput.addEventListener("input", receberQuantidadePessoas)
-
-function receberQuantidadePessoas(evento) {
-
-     const paragrafoErro = document.querySelector(".pessoas #erro")
-      const divErro = document.querySelector(".pessoas .input-box")
-
-    if(evento.target.value  === "0") {
-        paragrafoErro.style.display = "block"
-        divErro.setAttribute("id","erro-div")
-    } else{
-        paragrafoErro.style.display = "none"
-        divErro.setAttribute("id","")
-        pessoas = Number(evento.target.value)
+    function receberValorConta(evento) {
+        conta = Number(evento.target.value)
     }
-}
+
+    const pessoasInput = document.querySelector("#pessoas")
+    pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+    function receberQuantidadePessoas(evento) {
+
+        const paragrafoErro = document.querySelector(".pessoas #erro")
+        const divErro = document.querySelector(".pessoas .input-box")
+
+        if(evento.target.value  === "0") {
+            paragrafoErro.style.display = "block"
+            divErro.setAttribute("id","erro-div")
+        } else{
+            paragrafoErro.style.display = "none"
+            divErro.setAttribute("id","")
+            pessoas = Number(evento.target.value)
+        }
+    }
+
+    const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+    botoesGorjeta.forEach(botao =>{
+        botao.addEventListener("click", receberPorcentagem)
+    })
+
+    function receberPorcentagem(evento){
+        console.log(evento.target.value)
+    //porcentagem = Number(evento.target.value) /100
+    //console.log(porcentagem)
+    }
